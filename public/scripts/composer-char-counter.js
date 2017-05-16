1318ede46231deab560cfcca822531536a78022e
vagrant <vivien.fan.1224@gmail.com>
2017-05-16 18:55:13 +0000

    character counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".new-tweet form textarea").on("keyup", function(event) {
+    var limit = +$(this).closest(".new-tweet").data("limit");
+    var length = $(this).val().length;
+    var remain = limit - length;
+    var counter = $(this).closest(".new-tweet").find(".counter");
+    counter.text(remain);
+    if (remain < 0) {
+      counter.css("color", "red");
+    } else {
+      counter.css("color", "#244751");
+    }
+  });
+});
