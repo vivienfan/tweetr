987d3af36380dc374e767b347e35b1ff48386235
vagrant <vivien.fan.1224@gmail.com>
2017-05-17 05:03:49 +0000

    switching tweeter to mongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -1,25 +1,24 @@
 "use strict";
 
-// Simulates the kind of delay we see with network or filesystem operations
-const simulateDelay = require("./util/simulate-delay");
-
 // Defines helper functions for saving and getting tweets, using the database `db`
 module.exports = function makeDataHelpers(db) {
   return {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
-        callback(null, true);
-      });
+      db.collection("tweets").insertOne(newTweet);
+      callback(null, true);
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, array) => {
+        if (err) {
+          callback(err, null);
+          return;
+        }
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+        callback(null, array.sort(sortNewestFirst));
       });
     }
 
